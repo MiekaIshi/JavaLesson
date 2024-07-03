701c83f6529cd32234ac24b1fc2def22c0a94260
mieka <miekachan.5656@gmail.com>
2024-07-03 13:36:55 +0900

    ウインドウ表示

---
+++ GUI/sam1.java
@@ -0,0 +1,19 @@
+package GUI;
+
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import java.awt.Container;
+import java.awt.BorderLayout;
+
+class sam1{
+  public static void main(String args[]){
+    JFrame frame = new JFrame("MyTitle");
+    frame.setBounds(100, 100, 600, 400);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    JButton button = new JButton("Push");
+
+    frame.getContentPane().add(button, BorderLayout.NORTH);
+    frame.setVisible(true);
+  }
+}
