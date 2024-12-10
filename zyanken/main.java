f16b5d7fb032267552f39ba8e0db900fd53d6431
mieka <miekachan.5656@gmail.com>
2024-12-11 08:54:48 +0900

    2エラー処理追加とメソッド分割

--- zyanken/main.java
+++ zyanken/main.java
@@ -4,22 +4,31 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class main {
-    public static void main(String[] args) throws IOException { {
+    public static void main(String[] args) throws IOException { 
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+        Random rand = new Random();
+        int pc = rand.nextInt(3); 
+        
+        
+        judg(input() ,pc);
+    }
+
+    public static int input() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
         System.out.println("あなたの手を入力");
         System.out.println("0:グー　1：チョキ　2：パー");
         int player = Integer.parseInt(br.readLine());
-         
-        
 
-        Random rand = new Random();
-        int pc = rand.nextInt(3); 
         
-        judg(player ,pc);
+        if((player < 0) || (player > 2)){
+            System.out.println("値が不正です。終了します");
+            System.exit(0);
+        }
+        return player ;
+        
     }
-        
-} 
 
     public static void judg(int play ,int cpu){
         System.out.println(play +","+ cpu);
