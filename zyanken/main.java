65db4c42d667ce973ea018b26c87ac37843a793e
mieka <miekachan.5656@gmail.com>
2024-12-13 07:59:36 +0900

    ゲームの回数を入力できるようにした

--- zyanken/main.java
+++ zyanken/main.java
@@ -7,11 +7,24 @@ public class main {
     public static void main(String[] args) throws IOException { 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-        Random rand = new Random();
-        int pc = rand.nextInt(3); 
+        System.out.println("★★じゃんけんゲーム★★");
+        System.out.print("何回勝負にしますか？：");
+        int count = Integer.parseInt(br.readLine());
+        if(count < 1 ){
+            System.out.println("値が不正です。終了します");
+            System.exit(1);
+        }
+        for(int i = 1 ; i <=count ; i++){
+            System.out.println(i +"回戦");
+            int yourHand = input();
+
+            Random rand = new Random();
+            int pc = rand.nextInt(3); 
+            System.out.print("コンピューターの手：");
+            handDisplay(pc);
         
-        
-        judg(input() ,pc);
+            judg(yourHand , pc);
+        }
     }
 
     public static int input() throws IOException {
@@ -21,17 +34,43 @@ public class main {
         System.out.println("0:グー　1：チョキ　2：パー");
         int player = Integer.parseInt(br.readLine());
 
-        
         if((player < 0) || (player > 2)){
             System.out.println("値が不正です。終了します");
-            System.exit(0);
+            System.exit(1);
         }
+
+        System.out.print("あなたの手;");
+        handDisplay(player);
+
+       
+
+        
+        
         return player ;
         
     }
 
+    public static void handDisplay(int hand){
+        switch(hand){
+            case 0 :
+            System.out.println("グー");
+            break;
+
+            case 1 :
+            System.out.println("チョキ");
+            break;
+
+            case 2 :
+            System.out.println("パー    ");
+            break;
+
+            default:
+            System.out.println("値が不正です。終了します。");
+            System.exit(1);
+        }
+    }
     public static void judg(int play ,int cpu){
-        System.out.println(play +","+ cpu);
+        //System.out.println(play +","+ cpu);
         int ju = play - cpu ;
         if(ju == 0){
             System.out.println("あいこ");
